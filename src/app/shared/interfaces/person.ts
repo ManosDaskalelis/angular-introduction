f3ec26263f4ebe0060bf146b5f2d8a9b742f32ac
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2024-04-01 21:02:08 +0300

    simple datatable primer

--- src/app/shared/interfaces/person.ts
+++ src/app/shared/interfaces/person.ts
@@ -5,3 +5,714 @@ export interface Person {
     email: string;
     address: string;
 }
+
+export interface EPerson {
+    givenName: string;
+    surName: string;
+    age: string;
+    email: string;
+    education: string;
+}
+
+export const ManyPerson: EPerson[] = [
+    {
+      "givenName": "Emily",
+      "surName": "Johnson",
+      "age": "52",
+      "email": "emilynicole@gmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Tyler",
+      "surName": "Long",
+      "age": "70",
+      "email": "tlong97@rocketmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Danielle",
+      "surName": "Morgan",
+      "age": "49",
+      "email": "danielle73@outlook.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "David",
+      "surName": "Butler",
+      "age": "31",
+      "email": "d.a.butler@outlook.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Megan",
+      "surName": "Robinson",
+      "age": "36",
+      "email": "merobinson@gmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Taylor",
+      "surName": "Sanders",
+      "age": "59",
+      "email": "t_j_sanders88@live.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Jose",
+      "surName": "Brown",
+      "age": "74",
+      "email": "jose_brown34@yahoo.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Steven",
+      "surName": "Alexander",
+      "age": "27",
+      "email": "s_alexander53@live.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Mary",
+      "surName": "Russell",
+      "age": "30",
+      "email": "mary@rocketmail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Erin",
+      "surName": "Hill",
+      "age": "61",
+      "email": "erin.michelle.hill@outlook.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Daniel",
+      "surName": "Williams",
+      "age": "70",
+      "email": "daniel_michael_williams@rocketmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Amanda",
+      "surName": "Ross",
+      "age": "66",
+      "email": "amanda_ross@yahoo.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "John",
+      "surName": "Lopez",
+      "age": "43",
+      "email": "johnlopez@outlook.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Megan",
+      "surName": "Diaz",
+      "age": "76",
+      "email": "meganmariediaz@live.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Katherine",
+      "surName": "Gray",
+      "age": "77",
+      "email": "kjgray@rocketmail.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Kimberly",
+      "surName": "Price",
+      "age": "26",
+      "email": "kimberly.price@ymail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Mary",
+      "surName": "Sanchez",
+      "age": "50",
+      "email": "msanchez@ymail.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Melissa",
+      "surName": "Martinez",
+      "age": "37",
+      "email": "mgmartinez@gmail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Haley",
+      "surName": "Reed",
+      "age": "65",
+      "email": "haleymichellereed@live.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Austin",
+      "surName": "Price",
+      "age": "37",
+      "email": "a_l_price@live.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Jason",
+      "surName": "Brooks",
+      "age": "75",
+      "email": "jason_brooks@ymail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Layla",
+      "surName": "Lopez",
+      "age": "24",
+      "email": "layla.lopez@ymail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Samantha",
+      "surName": "Rodriguez",
+      "age": "40",
+      "email": "samantha24@live.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Erin",
+      "surName": "Torres",
+      "age": "19",
+      "email": "erin_torres@outlook.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Patrick",
+      "surName": "Brown",
+      "age": "44",
+      "email": "pbrown@live.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Lillian",
+      "surName": "Wilson",
+      "age": "77",
+      "email": "lillian_margaret@rocketmail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Sofia",
+      "surName": "Cook",
+      "age": "50",
+      "email": "sofiacook@hotmail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Timothy",
+      "surName": "Howard",
+      "age": "33",
+      "email": "t_j@aol.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Tyler",
+      "surName": "Lopez",
+      "age": "69",
+      "email": "tyler_andrew_lopez@live.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Andrew",
+      "surName": "Long",
+      "age": "62",
+      "email": "aelong@aol.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Jennifer",
+      "surName": "Martin",
+      "age": "52",
+      "email": "jmmartin34@rocketmail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Brittany",
+      "surName": "Bailey",
+      "age": "19",
+      "email": "brittany@aol.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Jason",
+      "surName": "Ward",
+      "age": "25",
+      "email": "j.j.ward73@gmail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Henry",
+      "surName": "Price",
+      "age": "48",
+      "email": "henryeugeneprice34@outlook.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Grace",
+      "surName": "Rodriguez",
+      "age": "68",
+      "email": "gracerodriguez39@ymail.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Tyler",
+      "surName": "Wilson",
+      "age": "61",
+      "email": "t_l_wilson@live.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Danielle",
+      "surName": "Washington",
+      "age": "51",
+      "email": "daniellewashington@outlook.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Amber",
+      "surName": "Martinez",
+      "age": "68",
+      "email": "amber_anne_martinez@aol.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Evelyn",
+      "surName": "Powell",
+      "age": "31",
+      "email": "er@ymail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Timothy",
+      "surName": "Roberts",
+      "age": "69",
+      "email": "timothy.w@hotmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Chelsea",
+      "surName": "Long",
+      "age": "42",
+      "email": "c_long63@gmail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Erin",
+      "surName": "Evans",
+      "age": "65",
+      "email": "erin.evans15@live.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Laura",
+      "surName": "Martin",
+      "age": "22",
+      "email": "l.martin83@yahoo.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "David",
+      "surName": "King",
+      "age": "65",
+      "email": "d_a_king30@outlook.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Steven",
+      "surName": "Ward",
+      "age": "75",
+      "email": "s.ward@gmail.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Rebecca",
+      "surName": "Nelson",
+      "age": "31",
+      "email": "rebecca_nelson@outlook.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Oliver",
+      "surName": "Morgan",
+      "age": "69",
+      "email": "oliver_morgan@ymail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Lillian",
+      "surName": "Wright",
+      "age": "35",
+      "email": "l_m_wright@ymail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Layla",
+      "surName": "Rivera",
+      "age": "35",
+      "email": "l_rivera@live.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Kevin",
+      "surName": "Wright",
+      "age": "44",
+      "email": "k_w_wright@yahoo.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Abigail",
+      "surName": "Jenkins",
+      "age": "45",
+      "email": "amjenkins@live.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Allison",
+      "surName": "Rivera",
+      "age": "62",
+      "email": "allisonruthrivera@live.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Katherine",
+      "surName": "Simmons",
+      "age": "37",
+      "email": "katherine@live.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Brian",
+      "surName": "Lewis",
+      "age": "54",
+      "email": "balewis5@rocketmail.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Kevin",
+      "surName": "Hall",
+      "age": "59",
+      "email": "khall@rocketmail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Evelyn",
+      "surName": "Reed",
+      "age": "36",
+      "email": "e.reed@aol.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Evelyn",
+      "surName": "Mitchell",
+      "age": "65",
+      "email": "e_m43@rocketmail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Eric",
+      "surName": "Russell",
+      "age": "63",
+      "email": "e_russell@hotmail.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Tiffany",
+      "surName": "Gray",
+      "age": "28",
+      "email": "tiffany_f_gray@aol.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Timothy",
+      "surName": "Reed",
+      "age": "34",
+      "email": "t_l_reed53@rocketmail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Elizabeth",
+      "surName": "Hughes",
+      "age": "77",
+      "email": "elizabeth_hughes@rocketmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Timothy",
+      "surName": "Scott",
+      "age": "53",
+      "email": "t.scott@outlook.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Julian",
+      "surName": "Gray",
+      "age": "70",
+      "email": "j.gray82@rocketmail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Jack",
+      "surName": "Roberts",
+      "age": "73",
+      "email": "j_roberts18@aol.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Lauren",
+      "surName": "Diaz",
+      "age": "44",
+      "email": "l.m.diaz95@hotmail.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Jose",
+      "surName": "James",
+      "age": "47",
+      "email": "jose@ymail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Robert",
+      "surName": "Young",
+      "age": "37",
+      "email": "robert_charles_young@rocketmail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Adam",
+      "surName": "Roberts",
+      "age": "75",
+      "email": "a.roberts27@aol.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Aubrey",
+      "surName": "Baker",
+      "age": "43",
+      "email": "a.m.baker@rocketmail.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Nathan",
+      "surName": "Williams",
+      "age": "43",
+      "email": "n.g.williams@aol.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Charles",
+      "surName": "Butler",
+      "age": "39",
+      "email": "charles_butler@outlook.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Isabella",
+      "surName": "Turner",
+      "age": "50",
+      "email": "iturner@live.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Amanda",
+      "surName": "Davis",
+      "age": "72",
+      "email": "a.l.davis@gmail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Steven",
+      "surName": "Miller",
+      "age": "74",
+      "email": "s.s@outlook.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Tiffany",
+      "surName": "Adams",
+      "age": "49",
+      "email": "t_m_adams@yahoo.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Anthony",
+      "surName": "Powell",
+      "age": "50",
+      "email": "a.powell@outlook.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Heather",
+      "surName": "Thomas",
+      "age": "51",
+      "email": "h_l@hotmail.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Emma",
+      "surName": "Clark",
+      "age": "64",
+      "email": "eclark1@gmail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Charles",
+      "surName": "Ramirez",
+      "age": "34",
+      "email": "charles_c_ramirez7@ymail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Eleanor",
+      "surName": "Jenkins",
+      "age": "72",
+      "email": "e.l.jenkins@live.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Eric",
+      "surName": "Green",
+      "age": "68",
+      "email": "eric.scott.green32@ymail.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Jessica",
+      "surName": "Murphy",
+      "age": "58",
+      "email": "jessicajmurphy@gmail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Megan",
+      "surName": "Miller",
+      "age": "67",
+      "email": "m.n.miller@rocketmail.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Jonathan",
+      "surName": "Alexander",
+      "age": "33",
+      "email": "j_e_alexander@gmail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Nicole",
+      "surName": "Garcia",
+      "age": "49",
+      "email": "nicolemarie@aol.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Samantha",
+      "surName": "Peterson",
+      "age": "32",
+      "email": "s.m.peterson@gmail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Sara",
+      "surName": "Perez",
+      "age": "46",
+      "email": "s_r_perez90@ymail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Sophia",
+      "surName": "Russell",
+      "age": "33",
+      "email": "sophia@ymail.com",
+      "education": "Master’s degree"
+    },
+    {
+      "givenName": "Jeremy",
+      "surName": "Robinson",
+      "age": "75",
+      "email": "j.f.robinson68@yahoo.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Jonathan",
+      "surName": "Wilson",
+      "age": "56",
+      "email": "jonathanwilson@aol.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Gabriel",
+      "surName": "Foster",
+      "age": "64",
+      "email": "g.foster@rocketmail.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Maria",
+      "surName": "White",
+      "age": "71",
+      "email": "maria_may_white34@outlook.com",
+      "education": "Bachelor’s degree"
+    },
+    {
+      "givenName": "Daniel",
+      "surName": "Bennett",
+      "age": "37",
+      "email": "d.m.bennett@hotmail.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Benjamin",
+      "surName": "Coleman",
+      "age": "27",
+      "email": "benjamincoleman@live.com",
+      "education": "Doctoral degree"
+    },
+    {
+      "givenName": "Emily",
+      "surName": "Bennett",
+      "age": "76",
+      "email": "e_bennett@outlook.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Megan",
+      "surName": "Smith",
+      "age": "71",
+      "email": "m_smith@gmail.com",
+      "education": "High school diploma or equivalent"
+    },
+    {
+      "givenName": "Melissa",
+      "surName": "Mitchell",
+      "age": "21",
+      "email": "mmitchell@live.com",
+      "education": "Less than high school"
+    },
+    {
+      "givenName": "Isabella",
+      "surName": "Howard",
+      "age": "69",
+      "email": "isabella_howard@rocketmail.com",
+      "education": "Associate degree"
+    },
+    {
+      "givenName": "Dylan",
+      "surName": "Jackson",
+      "age": "75",
+      "email": "dylan.jackson61@outlook.com",
+      "education": "Some college, no degree"
+    },
+    {
+      "givenName": "Zachary",
+      "surName": "Martinez",
+      "age": "52",
+      "email": "zacharythomasmartinez@live.com",
+      "education": "Associate degree"
+    }
+  ]
