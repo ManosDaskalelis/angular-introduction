abd1e7e2c78297322e1e4beadcb5b0e62ad61a98
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2024-04-01 17:40:24 +0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from '../app/components/welcome/welcome.component';
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'event-bind-example', component: EventBindExampleComponent},
+    {path: "welcome", component: WelcomeComponent},
+    {path: '', redirectTo: '/welcome', pathMatch: 'full'}
+];
